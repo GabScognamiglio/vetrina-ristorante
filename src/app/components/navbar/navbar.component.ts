081ad1a4addb75030b2c6115f68f23281c5fd0c4
GabScognamiglio <gabbo16s@gmail.com>
2024-09-13 15:29:49 +0200

    styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +10,31 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   currentTheme = 'dark'; // Imposta il tema di default
 
+  isTransparent: boolean = false;
+  isHome: boolean = false; // Per controllare se siamo nella home
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd)) // Solo quando la navigazione è finita
+      .subscribe(() => {
+        // Controlla se siamo nella Home
+        this.isHome = this.router.url === '/';
+        this.isTransparent = this.isHome; // Solo sulla home è trasparente
+      });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset = window.pageYOffset;
+
+    // Solo sulla home, cambia trasparenza in base allo scroll
+    if (this.isHome) {
+      this.isTransparent = scrollOffset <= 50; // Trasparente fino a 50px di scroll
+    }
+  }
+  
   toggleTheme() {
     this.currentTheme = this.currentTheme === 'light' ? 'dark' : 'light';
     document.documentElement.setAttribute('data-theme', this.currentTheme);
